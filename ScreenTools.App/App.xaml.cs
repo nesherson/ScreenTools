4d3375dbb789d7442fb27c3664dd1743d6504a92
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-01-12 16:17:53 +0100

    Add project files.

---
+++ ScreenTools.App/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace ScreenTools.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ScreenToolsTrayApp? trayApp;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            trayApp = new ScreenToolsTrayApp(this);
+
+            trayApp.Start();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            trayApp?.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ ScreenTools.App/ScreenToolsTrayApp.cs
@@ -0,0 +1,65 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ScreenTools.App
+{
+    public class ScreenToolsTrayApp : IDisposable
+    {
+        private const string trayIconPath = "pack://application:,,,/ScreenTools.App;component/Icons/trayIcon.ico";
+        private const string settingsIconPath = "pack://application:,,,/ScreenTools.App;component/Icons/settings.ico";
+        private const string exitIconPath = "pack://application:,,,/ScreenTools.App;component/Icons/exit.ico";
+
+        private readonly Application _application;
+
+        private TaskbarIcon? _trayIcon;
+
+        public ScreenToolsTrayApp(Application application)
+        {
+            _application = application;
+        }
+
+        public void Start()
+        {
+            _trayIcon = new TaskbarIcon
+            {
+                ToolTip = "Screen tools",
+                Icon = new BitmapImage(new Uri(trayIconPath)).ToIcon(),
+                ContextMenu = CreateContextMenu()
+            };
+        }
+
+        private ContextMenu CreateContextMenu()
+        {
+            var optionsIcon = new BitmapImage(new Uri(settingsIconPath)).ToIcon();
+            var exitIcon = new BitmapImage(new Uri(exitIconPath)).ToIcon();
+
+            return new ContextMenu
+            {
+                ItemsSource = new List<MenuItem>
+                {
+                    new() { Header = "Options", Icon = optionsIcon, Command = new RelayCommand(OpenOptions) },
+                    new() { Header = "Exit", Icon = exitIcon, Command = new RelayCommand(ExitApplication)}
+                }
+            };
+        }
+
+        public void Dispose()
+        {
+            _trayIcon?.Dispose();
+        }
+
+        private void ExitApplication()
+        {
+            _application.Shutdown();
+        }
+
+        private void OpenOptions()
+        {
+            _application.MainWindow = new MainWindow();
+            _application.MainWindow.Show();
+        }
+    }
+}
