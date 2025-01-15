8c465c9b427fb67e74e591dab3322df2d0be09c3
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-01-16 00:16:15 +0100

    Add simple drawing using Polyline

--- SystemTools.App/DrawingOverlay.axaml.cs
+++ SystemTools.App/DrawingOverlay.axaml.cs
@@ -1,11 +1,14 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace SystemTools.App;
 
 public partial class DrawingOverlay : Window
 {
+    Polyline? _currentPolyline;
     public DrawingOverlay()
     {
         InitializeComponent();
@@ -24,5 +27,45 @@ public partial class DrawingOverlay : Window
         WindowLockHook.Hook(this);
         
         base.OnLoaded(e);
+    }
+
+    private void Canvas_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        // var point = e.GetCurrentPoint(sender as Control);
+        
+        // if (point.Properties.IsLeftButtonPressed)
+        //     _currentPoint = point.Position;
+    }
+    
+    private void Canvas_OnPointerReleased(object? sender, PointerReleasedEventArgs e)
+    {
+        _currentPolyline = null;
+    }
+
+    private void Canvas_OnPointerMoved(object? sender, PointerEventArgs e)
+    {
+        var canvas = sender as Canvas;
+
+        if (canvas is null)
+            return;
+        
+        var point = e.GetCurrentPoint(canvas);
+
+        if (point.Properties.IsLeftButtonPressed)
+        {
+            if (_currentPolyline == null)
+            {
+                _currentPolyline = new Polyline
+                {
+                    Stroke = new SolidColorBrush(Colors.AliceBlue),
+                    StrokeThickness = 10
+                };
+                canvas.Children.Add(_currentPolyline);
+            }
+            else
+            {
+                _currentPolyline.Points.Add(point.Position);
+            }
+        }
     }
 }
