36fab4c04ee3cfaf53822f3de38a2eab473e0a0b
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-03-01 12:36:53 +0100

    Options view - WIP

---
+++ ScreenTools.App/Views/OptionsView.axaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Concurrency;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace ScreenTools.App;
+
+public partial class OptionsView : NotifyPropertyChangedWindowBase
+{
+    private ObservableCollection<string> _galleryPaths;
+    
+    public OptionsView()
+    {
+        InitializeComponent();
+
+
+       
+
+        RxApp.MainThreadScheduler.Schedule(LoadData);
+    }
+
+    public ObservableCollection<string> GalleryPaths
+    {
+        get => _galleryPaths;
+        set => SetField(ref _galleryPaths, value);
+    }
+
+    private void LoadData()
+    {
+        GalleryPaths = new ObservableCollection<string>()
+        {
+            "Test 1",
+            "Test 2"
+        };
+        this.Get<ListBox>("TestListBox").ItemsSource = GalleryPaths;
+       
+    }
+    
+}
