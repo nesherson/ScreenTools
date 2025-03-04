b5fc40aefccbd3000c61836cb3832deed5b2df38
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-03-04 01:20:48 +0100

    Add service for detecting text in image

--- ScreenTools.App/Extensions/ServiceCollectionExtensions.cs
+++ ScreenTools.App/Extensions/ServiceCollectionExtensions.cs
@@ -15,6 +15,7 @@ public static class ServiceCollectionExtensions
         collection.AddSingleton<SimpleGlobalHook>(_ => new SimpleGlobalHook(GlobalHookType.Keyboard));
         collection.AddTransient<WindowsToastService>();
         collection.AddTransient<ScreenCaptureService>();
+        collection.AddTransient<ImageProcessingService>();
         
         collection.AddSingleton<IConfiguration>(new ConfigurationBuilder()
             .AddJsonFile("appsettings.json").Build());
---
+++ ScreenTools.App/Services/ImageProcessingService.cs
@@ -0,0 +1,19 @@
+﻿using Tesseract;
+
+namespace ScreenTools.App;
+
+public class ImageProcessingService
+{
+    private const string TesseractLanguage = "eng";
+    private const string TesseractDataPath = "./tessdata";
+    
+    public string ProcessImage(byte[] imageBytes)
+    {
+        using var engine = new TesseractEngine(TesseractDataPath, TesseractLanguage, EngineMode.Default);
+        using var img = Pix.LoadFromMemory(imageBytes);
+        using var page = engine.Process(img);
+        var text = page.GetText();
+        
+        return text;
+    }
+}
