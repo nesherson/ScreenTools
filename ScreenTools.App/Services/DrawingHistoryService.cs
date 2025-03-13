9d95cdeb8d67dfe58f0bd83c37baaa85f2fe39ad
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-03-13 17:14:54 +0100

    Move undo functionality to service

---
+++ ScreenTools.App/Services/DrawingHistoryService.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Avalonia.Controls;
+
+namespace ScreenTools.App;
+
+public class DrawingHistoryService
+{
+    private readonly List<DrawingHistoryItem?> _drawingHistoryItems;
+
+    public DrawingHistoryService()
+    {
+        _drawingHistoryItems = [];
+    }
+    
+    public void Save(Control canvasControl, DrawingAction drawingAction)
+    {
+        Save([canvasControl], drawingAction);
+    }
+    
+    public void Save(List<Control> canvasControls, DrawingAction drawingAction)
+    {
+        _drawingHistoryItems.Add(new DrawingHistoryItem(canvasControls, drawingAction));
+    }
+    
+    public void Undo(Canvas canvas)
+    {
+        var itemToUndo = _drawingHistoryItems.LastOrDefault();
+
+        if (itemToUndo is null)
+            return;
+
+        switch (itemToUndo.Action)
+        {
+            case DrawingAction.Draw:
+                foreach (var canvasControl in itemToUndo.CanvasControls)
+                {
+                    canvas.Children.Remove(canvasControl);
+                }
+                _drawingHistoryItems.Remove(itemToUndo);
+                break;
+            case DrawingAction.Delete:
+            case DrawingAction.Clear:
+                canvas.Children.AddRange(itemToUndo.CanvasControls);
+                _drawingHistoryItems.Remove(itemToUndo);
+                break;
+        }
+    }
+
+}
