b6950c52ac2225edef43f889eb02f60632b6df6e
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-07-07 23:36:33 +0200

    Add ShapeTemplateSelector

---
+++ ScreenTools.App/DataTemplates/ShapeTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Metadata;
+using ScreenTools.Core;
+
+namespace ScreenTools.App;
+
+public class ShapeTemplateSelector : IDataTemplate
+{
+    [Content]
+    public Dictionary<string, IDataTemplate> Templates { get; } = new();
+
+    public Control Build(object? param)
+    {
+        if (param is null)
+        {
+            return new TextBlock { Text = "param is null" };
+        }
+
+        var type = param.GetType().Name;
+
+        if (Templates.TryGetValue(type, out var template))
+        {
+            return template.Build(param);
+        }
+
+        return new TextBlock { Text = $"No template found for {type}" };
+    }
+
+    public bool Match(object? data)
+    {
+        return data is ShapeViewModelBase;
+    }
+}
