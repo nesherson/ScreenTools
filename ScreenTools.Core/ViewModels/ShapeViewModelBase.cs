5e03c3ea04463f4a41bcab706ba2cb5cf4cd6826
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-07-09 23:46:37 +0200

    Add PointerPressedCommand

--- ScreenTools.Core/ViewModels/ShapeViewModelBase.cs
+++ ScreenTools.Core/ViewModels/ShapeViewModelBase.cs
@@ -1,5 +1,9 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace ScreenTools.Core;
 
-public class ShapeViewModelBase : ObservableObject {}
+public class ShapeViewModelBase : ObservableObject
+{
+    public ICommand? PointerPressedCommand { get; set; } 
+}
