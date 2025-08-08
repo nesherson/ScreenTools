e4b295a5f96f0acc78bb2d41b1bb806631c350a2
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-08-08 23:57:46 +0200

    Add BaseViewModel

---
+++ ScreenTools.App/ViewModels/BaseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avalonia.Controls.Notifications;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace ScreenTools.App;
+
+public class ViewModelBase : ObservableObject
+{
+    public void ShowWindowNotifcation(string title, string message, NotificationType type, Action? onClick = null)
+    {
+        WeakReferenceMessenger.Default
+            .Send(new ShowWindowNotificationMessage(
+                new Notification(title, message, type, null, onClick)));
+    }
+}
