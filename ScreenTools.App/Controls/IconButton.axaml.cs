aaa4073ddddf8f3e9c0aabaa876dd0e1c292a5ee
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-10-10 21:54:20 +0200

    Add IconColor property to IconButton

--- ScreenTools.App/Controls/IconButton.axaml.cs
+++ ScreenTools.App/Controls/IconButton.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace ScreenTools.App.Controls;
 
@@ -7,10 +8,19 @@ public class IconButton : Button
 {
     public static readonly StyledProperty<string> IconTextProperty = AvaloniaProperty.Register<IconButton, string>(
         nameof(IconText));
+    
+    public static readonly StyledProperty<IBrush> IconColorProperty = AvaloniaProperty.Register<IconButton, IBrush>(
+        nameof(IconColor), SolidColorBrush.Parse("#4f5d64"));
 
     public string IconText
     {
         get => GetValue(IconTextProperty);
         set => SetValue(IconTextProperty, value);
+    }
+    
+    public IBrush IconColor
+    {
+        get => GetValue(IconColorProperty);
+        set => SetValue(IconColorProperty, value);
     }
 }
