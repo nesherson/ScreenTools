d234ad018ca9337cb5bd8c195b14a170e2685107
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-10-18 00:08:07 +0200

    Uncomment drawing overlay code

--- ScreenTools.App/Views/DrawingOverlay.axaml.cs
+++ ScreenTools.App/Views/DrawingOverlay.axaml.cs
@@ -77,9 +77,9 @@ public partial class DrawingOverlay : ReactiveWindow<DrawingOverlayViewModel>
         if (ViewModel is null)
             return;
 
-        // canvas.PointerPressed += (_, pe) => ViewModel.OnPointerPressed(pe.GetCurrentPoint(Canvas));
-        // canvas.PointerMoved += (_, pe) => ViewModel.OnPointerMoved(pe.GetCurrentPoint(Canvas));
-        // canvas.PointerReleased += (_, _) => ViewModel.OnPointerReleased();
+        canvas.PointerPressed += (_, pe) => ViewModel.OnPointerPressed(pe.GetCurrentPoint(canvas));
+        canvas.PointerMoved += (_, pe) => ViewModel.OnPointerMoved(pe.GetCurrentPoint(canvas));
+        canvas.PointerReleased += (_, _) => ViewModel.OnPointerReleased();
     }
 
     private void OnHidden(EventArgs e)
